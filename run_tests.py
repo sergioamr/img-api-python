e80ba0610c32258cc060ee9ccf12d21250f3747c
Sergio A. Martinez <sergio@engineer.blue>
2022-06-10 11:58:26 +0000

    [UnitTests]
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,7 @@ from unittest import TestLoader, TextTestRunner, TestSuite
 
 loader = TestLoader()
 this_dir = os.path.dirname(__file__)
-tests = loader.discover(start_dir="tests/", pattern="*")
+tests = loader.discover(start_dir="tests/", pattern="*_test.py")
 suite = TestSuite(tests)
 
 runner = TextTestRunner(verbosity=2)
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -2,11 +2,8 @@ import unittest
 from tests import api
 
 class TestUser(unittest.TestCase):
-
-    def __init__(self, *args, **kwargs):
-        print(" IMGAPI Unit testing ")
-        super(TestUser, self).__init__(*args, **kwargs)
-
-    def setUp(self):
-        print(" SETUP ")
+    def test_get_token(self):
+        json = api.get_token()
+        self.assertEqual(json['status'], "success", "Token failed")
+        self.assertEqual(len(json['token']), 201)
 
