0a89da478b6410ec91b504d09cb95b463de2f7a9
Sergio A. Martinez <sergio@engineer.blue>
2022-06-11 21:38:53 +0000

    [Upload] Added multiple descriptions to files
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

--- tests/unit_upload_test.py
+++ tests/unit_upload_test.py
@@ -1,19 +1,26 @@
 import os
+
 import unittest
 from tests import api
-
 
 class TestUpload(unittest.TestCase):
 
     def test_upload_files(self):
+        import json
 
         basepath = "tests/upload/"
         file_list = []
+        data = {}
+
+        idx = 0
         for entry in os.listdir(basepath):
+            data[entry] = json.dumps({"my_title": str(idx) + ":: My test title"})
             file_list.append(os.path.join(basepath, entry))
+            idx += 1
 
-        json = api.api_upload(file_list, data={"my_title": "Test Upload"})
-        self.assertEqual(json['status'], "success", "Upload failed")
+        json_res = api.api_upload(file_list, data=data)
+        self.assertEqual(json_res['status'], "success", "Upload failed")
 
-        media = json['media_files'][0]
-        self.assertEqual(media['my_title'], "Test Upload", "Failed to update title")
+        media = json_res['media_files'][0]
+        self.assertEqual(media['my_title'], "0:: My test title",
+                         "Failed to update title")
