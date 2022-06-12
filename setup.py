1b1483496ad37937e0abec2605e446e5e9e28bab
Sergio A. Martinez <sergio@engineer.blue>
2022-06-12 16:14:57 +0000

    [Module] Installer
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='imgapi',
+      version='0.1',
+      description='Img-api.com installed',
+      url='https://github.com/sergioamr/img-api-python',
+      author='Sergio A. Martinez',
+      author_email='contact@img-api.com',
+      license='MIT',
+      packages=['imgapi'],
+      zip_safe=False)
