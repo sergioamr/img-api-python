80093f8fd18c249cf97cbff33e4eb7492202409f
Manel Villar <manelvf@gmail.com>
2024-11-14 16:13:21 +0100

    Parse articles

--- run_fetch_stocktwits.py
+++ run_fetch_stocktwits.py
@@ -32,8 +32,98 @@ def init(api):
 
     driver = get_webdriver()
     news = get_news(driver, symbol)
-    print(news)
+
+    articles = []
+
+    for link in news:
+        if is_news(link):
+            log.info("Link already exists")
+            continue
+
+        article = get_article(driver, link)
+        print(article)
+        break
+
+    driver.close()
     
+def get_article(driver, link):
+    driver.get(link)
+
+    try:
+        # Check first if we are in the consent page (EU)
+        current_url = driver.execute_script("return window.location.href;")
+
+        if 'consent' in current_url:
+            print(" CONSENT URL:", current_url)
+            # We get the consent and click on it
+
+            accept_element = EC.element_to_be_clickable(
+                (By.CLASS_NAME, "accept-all"))
+
+            link = WebDriverWait(driver, 5).until(accept_element)
+            link.click()
+
+    except Exception as e:
+        print(e, "CRASHED READING CONSENT")
+
+    try:
+        current_url = driver.execute_script("return window.location.href;")
+        print(" URL:", current_url)
+
+        element_present = EC.presence_of_element_located(
+            (By.TAG_NAME, 'main'))
+
+        WebDriverWait(driver, 5).until(element_present)
+
+        buttons = driver.find_elements(By.TAG_NAME, "button")
+        print(f"Found {len(buttons)} buttons on the main page.")
+        for index, button in enumerate(buttons):
+            try:
+                if not button.text: continue
+                if button.get_attribute("type") == "submit": continue
+
+                cl = button.get_attribute("class")
+                print(
+                    f"Button {index + 1} Text: {button.text} {cl} "
+                )
+            except Exception as e:
+                print(e, "CRASH")
+
+        try:
+            link = driver.find_element(By.CLASS_NAME, "readmoreButtonText")
+            if link:
+                link.click()
+        except Exception as e:
+            print(" NO READ MORE BUTTON ")
+
+    except Exception as e:
+        print(e, "CRASHED")
+
+    try:
+        article = driver.find_element(By.TAG_NAME, "article")
+        article = article.text
+
+    except:
+        print("article tag not found")
+        article = ""
+        paragraphs = driver.find_elements(By.TAG_NAME, "p")
+        for paragraph in paragraphs:
+            article += paragraph.text
+
+    return article
+
+
+def clean_article(article):
+    """Cleans \n character from article"""
+
+    article = re.sub("\n", " ", article)
+    return article
+
+
+def is_news(link):
+    api_params = f"?link__icontains={link}"
+    json_in = api.api_call("/news/query" + api_params)
+    return bool(json_in["news"])
 
 
 def get_news(driver, symbol):
