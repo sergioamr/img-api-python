86ab5c15f40bc2db11b4789f0d9f7140cc2b2922
Sergio A. Martinez <sergio@engineer.blue>
2024-12-30 11:06:13 +0000

    [REDDIT] Download reddit posts
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

--- run_fetch.py
+++ run_fetch.py
@@ -86,139 +86,53 @@ def clean_article(article):
     return article
 
 
-def yfetch_process_news(api, item):
 
-    print_g(" NEWS -> " + item['link'])
+##############################################################################################
+import praw
 
-    driver = get_webdriver()
+reddit = praw.Reddit(
+    client_id=api.reddit_id,
+    client_secret=api.reddit_secret,
+    user_agent="testscript by u/tothemoon_life",
+)
+print(reddit.auth.scopes())
 
-    articles = []
+subreddit_name = "investing"
+subreddit = reddit.subreddit(subreddit_name)
 
-    print_b(" PUBLISHER " + item['publisher'])
-    if item["publisher"] not in [
-            "Barrons", "Financial Times", "The Information", "MT Newswires",
-            "Investor's Business Daily", "Yahoo Finance Video"
-    ]:
-        driver.get(item["link"])
+# Fetch the top posts
+posts = []
 
+# TOP
+for submission in subreddit.new(limit=10):  # Adjust the limit as needed
+
+    comment_list = []
+    for comment in submission.comments:
         try:
-            # Check first if we are in the consent page (EU)
-            current_url = driver.execute_script("return window.location.href;")
-
-            if 'consent' in current_url:
-                print(" CONSENT URL:", current_url)
-                # We get the consent and click on it
-
-                accept_element = EC.element_to_be_clickable(
-                    (By.CLASS_NAME, "accept-all"))
-
-                link = WebDriverWait(driver, 5).until(accept_element)
-                link.click()
-
-        except Exception as e:
-            print_exception(e, "CRASHED READING CONSENT")
-            pass
-
-        try:
-            current_url = driver.execute_script("return window.location.href;")
-            print(" URL:", current_url)
-
-            element_present = EC.presence_of_element_located(
-                (By.TAG_NAME, 'main'))
-
-            WebDriverWait(driver, 5).until(element_present)
-
-            buttons = driver.find_elements(By.TAG_NAME, "button")
-            print(f"Found {len(buttons)} buttons on the main page.")
-            for index, button in enumerate(buttons):
-                try:
-                    if not button.text: continue
-                    if button.get_attribute("type") == "submit": continue
-
-                    cl = button.get_attribute("class")
-                    print(
-                        f"Button {index + 1} Text: {Fore.BLUE} {button.text} {Style.RESET_ALL} {cl} "
-                    )
-                except Exception as e:
-                    print_exception(e, "CRASH")
-
-            try:
-                link = driver.find_element(By.CLASS_NAME, "readmoreButtonText")
-                if link:
-                    link.click()
-            except Exception as e:
-                print_r(" NO READ MORE BUTTON ")
-
+            if comment.author:
+                comment_list.append({
+                    "body": comment.body,
+                    "author": comment.author.name,
+                    "created": comment.created,
+                })
         except Exception as e:
             print_exception(e, "CRASHED")
 
-        try:
-            article = driver.find_element(By.TAG_NAME, "article")
-            article = article.text
+    posts.append({
+        "title": submission.title,
+        "text": submission.selftext,
+        "score": submission.score,
+        "url": submission.url,
+        "id": submission.id,
+        "author": str(submission.author),
+        "comments": submission.num_comments,
+        "comment_list": comment_list
+    })
 
-        except:
-            print("article tag not found", item["publisher"])
-            article = ""
-            paragraphs = driver.find_elements(By.TAG_NAME, "p")
-            for paragraph in paragraphs:
-                article += paragraph.text
-            article = clean_article(article)
-        finally:
-            articles.append(article)
+import json
+with open(f"{subreddit_name}_new_posts.json", "w", encoding="utf-8") as file:
+    json.dump(posts, file, indent=4, ensure_ascii=False)
 
-        driver.close()
-    else:
-        if item["publisher"] in ["Barrons", "MT Newswires"]:
-            print_e(" TO BE IMPLEMENTED ")
-
-        elif item["publisher"] == "Investor's Business Daily":
-            try:
-                print_e(" TO BE IMPLEMENTED ")
-            except Exception as e:
-                print_exception(e, " FAILED ")
-
-    driver.quit()
-
-    # Reindex because we haven't finish this code
-
-    if len(articles) > 0:
-        item['articles'] = articles
-        item['state'] = "INDEXED"
-    else:
-        item['state'] = "ERROR: ARTICLES NOT FOUND"
-
-    return item
-
-
-##############################################################################################
-
-api_params = "?status=WAITING_INDEX&limit=1&source=YFINANCE&publisher=GlobeNewswire"
-
-json_in = api.api_call("/news/query" + api_params)
-print_b(json.dumps(json_in, indent=4))
-
-for article in json_in['news']:
-    print_b(" FETCH LINK " + article['link'])
-    update = yfetch_process_news(api, article)
-
-    if update:
-        # Api expects a list of news articles, we can batch later the calls
-        js_dict = {'news': [update]}
-
-        print(json.dumps(js_dict, indent=4))
-        res = api.api_call("/news/update", data=js_dict)
-
-        if not res:
-            print_e(" FAILED COMMUNICATING WITH API ")
-
-        elif res['status'] == "error":
-            print_e(" API ERROR " + res['error_msg'])
-
-        elif res['status'] != "success":
-            print_r(json.dumps(res, indent=4))
-            print_e(" FAILED UPDATING ")
-
-        else:
-            print_g(" UPDATE SUCCESSFUL ")
+print(f"Downloaded {len(posts)} posts from r/{subreddit_name}!")
 
 print_b(" FETCH FINISHED ")
